a4fbedc502239e3cb99bba893d12401990c53380
Luana Cardoso <luanacoder@gmail.com>
2022-05-21 23:49:53 -0300

    Utilizando flexbox na estilização + ajustes nos arquivos html e js

--- js/script.js
+++ js/script.js
@@ -2,33 +2,30 @@ let weight= document.querySelector('#inputWeight');
 let height = document.querySelector('#inputHeight');
 let result = document.querySelector('#result');
 let calculateBtn = document.querySelector('#calculate');
-let classification = document.querySelector('#classification');
 
 
-    var imc = weight.value/(height.value * height.value);
-    result.textContent = `Seu IMC é ${imc.toFixed(2)}`;
-
-    !height || !weight ? result.textContent = "" : result.textContent = `Seu IMC é ${imc.toFixed(2)}`;
-
-    if(!imc){
-        classification.textContent = "";
-    }
-    else if(imc <= 18.5){
-        classification.textContent = "Situação: Abaixo do peso";
+function calculateImc() {
+    const imc = weight.value/(height.value * height.value);
+    let classification = ""
+    
+    if(imc <= 18.5){
+        classification = "Situação: Abaixo do peso";
     }
     else if (imc >= 18.6 && imc <= 24.9){
-        classification.textContent = "Situação: Normal";
+        classification = "Situação: Normal";
     }
     else if (imc >= 25 && imc <= 29.9){
-        classification.textContent = "Situação: Sobrepeso";
+        classification = "Situação: Sobrepeso";
     }
     else if (imc >= 30 && imc <= 34.9){
-        classification.textContent = " Situação: Obesidade classe I";
+        classification = " Situação: Obesidade classe I";
     }
     else if (imc >= 35 && imc <= 39.9){
-        classification.textContent = " Situação: Obesidade classe II";
+        classification = " Situação: Obesidade classe II";
     }else{
-        classification.textContent = " Situação: Obesidade classe III";
+        classification = " Situação: Obesidade classe III";
     }
 
+    height.value == "" || weight.value == "" ? result.textContent = "" : result.textContent = `Seu IMC é ${imc.toFixed(2)} ${classification}`;
 
+}
